4c88ac34405ef30e991f154fda859aa09583f42a
santiago bertinat <santiago.bertinat@gmail.com>
2022-01-16 19:23:22 -0300

    Change install generator to create styleguide layout file

--- lib/generators/bootstrap_styleguide/install_generator.rb
+++ lib/generators/bootstrap_styleguide/install_generator.rb
@@ -2,11 +2,16 @@ require 'rails/generators'
 
 module BootstrapStyleguide
   class InstallGenerator < Rails::Generators::Base
-    source_root File.expand_path('../../../app/views/bootstrap_styleguide', __dir__)
+    source_root File.expand_path('../../../app/views', __dir__)
 
     def add_routes
       styleguide_routes = "mount BootstrapStyleguide::Engine => '/bootstrap_styleguide'"
       route styleguide_routes
+    end
+
+    def create_styleguide_layout
+      layout_path = File.expand_path('../../../app/views/layouts/styleguide.html.erb', __dir__)
+      copy_file layout_path, "app/views/layouts/styleguide.html.erb"
     end
 
     def copy_styleguide_views
