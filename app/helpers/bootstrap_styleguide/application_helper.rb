058f16462aa5ac2fe3cef0c44c8beaf527a145a2
santiago bertinat <santiago.bertinat@gmail.com>
2022-01-17 21:15:15 -0300

    Add home view with the list of the styleguide's elements and components

--- app/helpers/bootstrap_styleguide/application_helper.rb
+++ app/helpers/bootstrap_styleguide/application_helper.rb
@@ -1,5 +1,29 @@
 module BootstrapStyleguide
   module ApplicationHelper
+    def layout_items
+      [:breakpoints, :containers, :grid]
+    end
+
+    def content_items
+      [:typography, :images, :tables]
+    end
+
+    def forms_items
+      [:select, :checks_and_radios, :range, :input_group, :floating_labels]
+    end
+
+    def components_items
+      [
+        :accordion, :alerts, :badge, :breadcrumbs, :buttons, :button_group, :cards, :carousel,
+        :close_button, :collapse, :dropdowns, :modal, :navs_and_tabs, :navbar, :offcanvas, :pagination,
+        :popovers, :progress, :spinners, :toasts, :tooltips
+      ]
+    end
+
+    def utilities_items
+      [:background, :borders, :colors, :shadows, :spacing, :text]
+    end
+
     def example_with_code(&block)
       content = capture(&block)
 
